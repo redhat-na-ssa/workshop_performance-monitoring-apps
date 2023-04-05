67fb581632aa1c483ecd7beb4e8a3bd4d868d9bb
Rafael T. C. Soares <rafaelcba@gmail.com>
2023-04-04 22:28:28 -0400

    fixing Micronaut App name

--- micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/MicronautApplication.java
+++ micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/MicronautApplication.java
@@ -2,9 +2,9 @@ package io.containerapps.javaruntime.workshop.micronaut;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class MicronautApplication {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        Micronaut.run(MicronautApplication.class, args);
     }
 }
