734d3f7f52ab04f0297d7f3f616717f54b44934e
Rafael T. C. Soares <rafaelcba@gmail.com>
2023-04-07 11:25:39 -0400

    improving PG Cluster script

--- infrastructure/deploy_pg_using_operator.sh
+++ infrastructure/deploy_pg_using_operator.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OPERATOR_API_GROUP="postgres-operator.crunchydata.com"
+POSTGRES_INSTANCE_NAME="postgres"
 oc project ${DEVWORKSPACE_NAMESPACE}
 
 echo
@@ -15,11 +16,11 @@ fi
 
 echo
 echo "Check if there is PG Cluster already deployed."
-oc get PostgresCluster postgres
+oc get PostgresCluster $POSTGRES_INSTANCE_NAME
 if [[ $? -eq 0 ]] 
 then
    echo
-   echo "Resource pg-cluster already present in this project!!!"
+   echo "Resource $POSTGRES_INSTANCE_NAME already present in this project!!!"
    exit 1
 fi
 
@@ -34,11 +35,18 @@ then
   echo
   echo "Wait until PG instance gets ready..."
   sleep 3
-  podname=$(oc get pod -o name | grep pod/postgres-instance1)
+  podname=$(oc get pod -o name | grep pod/$POSTGRES_INSTANCE_NAME-instance1)
   oc wait --for=condition=Ready $podname
 
+  # Annotate PG cluster resources to group them
+  oc label statefulsets \
+    -l postgres-operator.crunchydata.com/cluster=$POSTGRES_INSTANCE_NAME app.kubernetes.io/part-of=Postgres-Cluster
+  oc label jobs \
+    -l postgres-operator.crunchydata.com/cluster=$POSTGRES_INSTANCE_NAME app.kubernetes.io/part-of=Postgres-Cluster
+
   # change the auto-generated SCRAM based db password
-  oc patch secret postgres-pguser-postgres -p '{"stringData":{"password":"password","verifier":""}}'
+  oc patch secret $POSTGRES_INSTANCE_NAME-pguser-$POSTGRES_INSTANCE_NAME \
+    -p '{"stringData":{"password":"password","verifier":""}}'
 
   echo "then you can connect to it using the following properties:"
   echo
@@ -51,12 +59,12 @@ then
 #       uri: .data.uri | @base64d
 #       }' > ${PROJECT_SOURCE:-$(pwd)}/pg-conn-info.json
 
-  oc get secret postgres-pguser-postgres -o json | jq '{ 
+  oc get secret $POSTGRES_INSTANCE_NAME-pguser-$POSTGRES_INSTANCE_NAME -o json | jq '{ 
       user: .data.user | @base64d, 
       password: .data.password | @base64d, 
-      host: "postgres-ha", 
+      host: "${POSTGRES_INSTANCE_NAME}-ha", 
       dbname: .data.dbname | @base64d, 
-      uri: "jdbc:postgresql://postgres-ha/postgres"
+      uri: "jdbc:postgresql://${POSTGRES_INSTANCE_NAME}-ha/postgres"
       }' > ${PROJECT_SOURCE:-$(pwd)}/pg-conn-info.json
 
   cat ${PROJECT_SOURCE:-$(pwd)}/pg-conn-info.json
