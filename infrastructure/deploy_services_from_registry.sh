538b5b6f066afe444cb7b4f41544e386ac3bc2b1
Rafael T. C. Soares <rafaelcba@gmail.com>
2023-04-24 12:29:24 -0400

    changing pipeline to handle one app at a time

--- infrastructure/deploy_services_from_registry.sh
+++ infrastructure/deploy_services_from_registry.sh
@@ -11,14 +11,23 @@ kn service create quarkus-app \
  --probe-readiness-opts='initialDelaySeconds=3,periodSeconds=10,failureThreshold=3,successThreshold=1,timeoutSeconds=10' \
  --request=cpu=200m,memory=128Mi \
  --limit=cpu=2,memory=1.5Gi \
- --env=JAVA_MAX_MEM_RATIO=70.0 \
- --env=GC_MAX_HEAP_FREE_RATIO=30 \
+ --env=JAVA_MAX_MEM_RATIO=75.0 \
+ --env=GC_MAX_HEAP_FREE_RATIO=25 \
  --env=QUARKUS_LOG_CONSOLE_JSON=false \
  --label=app.openshift.io/runtime=quarkus \
  --scale=0..3 \
  --no-wait \
  --force
- 
+# use when # of cpu >= 2 and heap mem <=4gb
+# --env=GC_CONTAINER_OPTIONS='-XX:+UseSerialGC' \
+# --env=GC_CONTAINER_OPTIONS=-XX:+UseParallelGC \
+        # - name: GC_CONTAINER_OPTIONS
+        #   value: -XX:+UseSerialGC
+        # - name: JAVA_MAX_MEM_RATIO
+        #   value: "75"
+        # - name: JAVA_OPTS_APPEND
+        #   value: "-XX:ActiveProcessorCount=2"
+
  kn service create micronaut-app \
  --image=quay.io/rafaeltuelho/micronaut-app:latest \
  --probe-liveness='http:::/health' \
@@ -27,8 +36,8 @@ kn service create quarkus-app \
  --probe-readiness-opts='initialDelaySeconds=3,periodSeconds=15,failureThreshold=3,successThreshold=1,timeoutSeconds=10' \
  --request=cpu=200m,memory=128Mi \
  --limit=cpu=2,memory=1.5Gi \
- --env=JAVA_MAX_MEM_RATIO=85.0 \
- --env=GC_MAX_HEAP_FREE_RATIO=30 \
+ --env=JAVA_MAX_MEM_RATIO=75.0 \
+ --env=GC_MAX_HEAP_FREE_RATIO=25 \
  --label=app.openshift.io/runtime=java \
  --scale=0..3 \
  --no-wait \
@@ -43,8 +52,8 @@ kn service create quarkus-app \
  --request=cpu=200m,memory=128Mi \
  --limit=cpu=2,memory=1.5Gi \
  --env=spring_profiles_active=production \
- --env=JAVA_MAX_MEM_RATIO=80.0 \
- --env=GC_MAX_HEAP_FREE_RATIO=20 \
+ --env=JAVA_MAX_MEM_RATIO=75.0 \
+ --env=GC_MAX_HEAP_FREE_RATIO=25 \
  --label=app.openshift.io/runtime=spring-boot \
  --scale=0..3 \
  --no-wait \
